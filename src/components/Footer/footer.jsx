a091b5c54e0b5485810f308b5d14c9e438321939
Nikhil Kumar Sharma <nikhilkumarsharman815@gmail.com>
2024-05-10 12:29:47 +0530

    Edited Footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -10,28 +10,60 @@ export default function footer() {
           <div className="footer-logo">
             <img src={logo} alt="footer-logo" />
           </div>
-          <div className="footer-nav">
-            <ul>
+          <div className="footer-nav-col">
+            <div className="footer-nav">
+              <ul>
+                <li><b>About Us</b></li>
                 <li>Home</li>
-                <li>About Us</li>
                 <li>Contact Us</li>
-            </ul>
+                <li>FAQs</li>
+                <li>Support</li>
+                <li>Blog</li>
+              </ul>
+            </div>
+            <div className="footer-nav">
+              <ul>
+                <li><b>Services</b></li>
+                <li>Influencers</li>
+                <li>Brands</li>
+                <li>Partners</li>
+                <li>About</li>
+                <li>Team</li>
+              </ul>
+            </div>
+            <div className="footer-nav">
+              <ul>
+                <li><b>Careers</b></li>
+                <li>Press</li>
+                <li>Events</li>
+                <li>Help</li>
+                <li>Terms</li>
+                <li>Privacy</li>
+              </ul>
+            </div>
           </div>
-          <div className="footer-nav">
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-            </ul>
-          </div>
-          <div className="footer-nav">
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-            </ul>
+          <div className="footer-form">
+            <h3>Subscribe</h3>
+            <p>Join our mailing list for latest updates.</p>
+            <div className="footer-form-inp">
+              <input id="footer-form-inpBox" type="email" placeholder="Enter email adress" />
+              <button type="submit">Submit</button>
+            </div>
+            <p>
+              By, subscribing you agree to our Privacy Policy and consent to
+              receive updates.
+            </p>
           </div>
         </div>
+        <div className="copyright-sec">
+            <hr />
+            <div className="copyrightText">
+              <span>&copy; 2024 <b>APTA </b> All rights reserved</span>
+              <p>Privacy Policy</p>
+              <p>Terms of Services</p>
+              <p>Cookie Settings</p>
+            </div>
+          </div>
       </footer>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,22 +10,19 @@ import Hero2 from "../assets/Hero/Hero2.jpg";
 import Hero3 from "../assets/Hero/Hero3.jpg";
 
 export default function Home() {
-  const[hero, setHero] = useState(0)
-  const heroImages = [
-    Hero2,
-    Hero3
-  ]
+  const [hero, setHero] = useState(0);
+  const heroImages = [Hero2, Hero3];
 
-  useEffect(()=>{
-    const intervalId = setInterval(()=> {
-      setHero((preHero) => (preHero+1)%heroImages.length)
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setHero((preHero) => (preHero + 1) % heroImages.length);
     }, 3000);
 
-    return() => clearInterval(intervalId);
-  }, [heroImages.length])
-  
+    return () => clearInterval(intervalId);
+  }, [heroImages.length]);
+
   return (
-    <div className="home-page" >
+    <div className="home-page">
       <section className="hero">
         <div className="hero-frame">
           <img src={heroImages[hero]} alt={`Slide ${hero + 1}`} />
@@ -46,43 +43,11 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="home-sec2">
-        <div className="cards-container">
-          <div className="card">
-            <div className="card-icon">
-              <img src={diamond_svg} alt="" />
-            </div>
-            <div className="content">
-              <h2>Connecting Influencers with Companies and More</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              <Link to="#">Learn More</Link>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-icon">
-              <img src={compass_svg} alt="" />
-            </div>
-            <div className="content">
-              <h2>Discover New Sponsorship Opportunities</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              <Link to="#">Learn More</Link>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-icon">
-              <img src={people_svg} alt="" />
-            </div>
-            <div className="content">
-              <h2>Connect and Collaborate with Influencers</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-              <Link to="#">Learn More</Link>
-            </div>
-          </div>
-        </div>
-      </section>
-
       <section className="home-sec3">
-        <h1>Discover Influencers and Companies for<br /> Sponsorships</h1>
+        <h1>
+          Discover Influencers and Companies for
+          <br /> Sponsorships
+        </h1>
         <div className="cards-container-2">
           <div className="card2">
             <div className="card2-icon">
@@ -129,10 +94,52 @@ export default function Home() {
         </div>
       </section>
 
+      {/* Changed positionof home-sec2  */}
+      <section className="home-sec2">
+        <div className="cards-container">
+          <div className="card">
+            <div className="card-icon">
+              <img src={diamond_svg} alt="" />
+            </div>
+            <div className="content">
+              <h2>Connecting Influencers with Companies and More</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <Link to="#">Learn More</Link>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-icon">
+              <img src={compass_svg} alt="" />
+            </div>
+            <div className="content">
+              <h2>Discover New Sponsorship Opportunities</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <Link to="#">Learn More</Link>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-icon">
+              <img src={people_svg} alt="" />
+            </div>
+            <div className="content">
+              <h2>Connect and Collaborate with Influencers</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <Link to="#">Learn More</Link>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section className="home-sec4">
-        <h1>Connect and Collaborate<br /> with Influencers</h1>
+        <h1>
+          Connect and Collaborate
+          <br /> with Influencers
+        </h1>
         <div className="home-sec4-cta">
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum quisquam in nisi expedita, repellendus aliquid.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+            quisquam in nisi expedita, repellendus aliquid.
+          </p>
           <button>Sign Up</button>
           <button>Login</button>
         </div>
